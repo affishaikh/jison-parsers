a4e1221cd34f8e164c84c43fa0f1299ae79db773
affishaikh <aftabshk@In-Aftabshk.local>
2020-02-14 16:16:13 +0530

    Create validator for numbers, words

--- my-parser.js
+++ my-parser.js
@@ -4,12 +4,13 @@ const grammar = {
   lex: {
     rules: [
       ["\\s+", "return 'SPACE';"],
-      ["[A-z0-9]+", "return 'Word';"]
+      ["[A-z0-9]*[A-z]+[0-9]*", "return 'Word';"],
+      ["[0-9]+", "return 'Number';"]
     ]
   },
 
   bnf: {
-    SentenceWithNumbers: ["SentenceWithNumbers SPACE Word", "Word"]
+    SentenceWithNumbers: ["SentenceWithNumbers SPACE Word", "Word", "Number"]
   }
 };
 
@@ -17,6 +18,5 @@ const parser = new Parser(grammar);
 
 console.log(parser.parse("string"));
 console.log(parser.parse("some     big sentence like this"));
-console.log(parser.parse("420 k"));
-console.log(parser.parse("khiladi 420 k"));
 console.log(parser.parse("420k"));
+console.log(parser.parse("888"));
